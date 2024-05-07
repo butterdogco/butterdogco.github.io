1cb4f89639d26a5d982f558151a88ff503d7f9d5
ButterDogCEO <100174775+butterdogceo@users.noreply.github.com>
2024-05-07 13:43:47 -0700

    fix | maybe fix alt links

--- js/apps.js
+++ js/apps.js
@@ -2,7 +2,7 @@ const link = window.location.href;
 if (!link.includes("https://b-dog.co")) {
     var things = document.querySelectorAll("a");
     things.forEach(obj => {
-        if (obj.parentElement.className = "card") {
+        if (obj.parentNode.className = "card") {
             if (obj.getAttribute('data-alt-link')) {
                 obj.href = obj.getAttribute('data-alt-link');
                 obj.title = "Click to open alternate link";
