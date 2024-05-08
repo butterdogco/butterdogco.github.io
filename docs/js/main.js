fa148c28f4760a25dc4079f409f68eae2f8c6e84
ButterDogCEO <100174775+butterdogceo@users.noreply.github.com>
2024-05-08 12:49:56 -0700

    fix | fixed the background image and stuff

--- docs/js/main.js
+++ docs/js/main.js
@@ -37,6 +37,15 @@ if (!link.includes("https://b-dog.co")) {
       }
     }
   }
+
+  var things = document.querySelectorAll("a");
+  var things_array = [...things];
+  things_array.forEach(obj => {
+    if (obj.getAttribute('data-alt-link')) {
+        obj.href = obj.getAttribute('data-alt-link');
+        obj.title = "Click to open alternate link";
+    }
+  });
 } else if (link.includes("/altsite")) {
   window.location.href = "index";
 }
