02105517cc2351fee65c3fc39da777e60f16f640
ButterDogCEO <100174775+butterdogceo@users.noreply.github.com>
2024-09-06 14:26:25 -0700

    UDPATE!| NEW APP CHECK IT OUT RIGHT NOW

--- docs/js/cards.js
+++ docs/js/cards.js
@@ -1,14 +1,14 @@
 const cards = {
   ["home"]:[
     {
-      Name:"Make a search with Boogle",
+      Name:"make a search with Boogle",
       Desc:"",
       Icon:"img/icons/Boogle.png",
       Link:"https://b-dog.co/Boogle-2.0/",
     },
     
     {
-      Name:"Learn more with Wikabedia",
+      Name:"learn more with Wikabedia",
       Desc:"",
       Icon:"img/icons/Wikabedia.png",
       Link:"https://wikabedia.b-dog.co",
@@ -16,7 +16,7 @@ const cards = {
     },
     
     {
-      Name:"Check out our other apps",
+      Name:"check out our other apps",
       Desc:"",
       Icon:"img/apps.png",
       Link:"applications",
@@ -24,75 +24,81 @@ const cards = {
   ],
   ["apps"]:[
     {
-      Name:"Boogle",
-      Desc:"Da best serch engun",
+      Name:"boogle",
+      Desc:"da best serch engun",
       Icon:"img/icons/Boogle.png",
       Link:"https://boogle.b-dog.co"
     },
     {
-      Name:"Wikabedia",
-      Desc:"Wikabedia Uplode epik artikles instentley!",
+      Name:"wikabedia",
+      Desc:"wikabedia uplode epik artikles instentley!",
       Icon:"img/icons/Wikabedia.png",
       Link:"https://wikabedia.b-dog.co",
       AltLink:"https://butterdogceo.github.io/Wikabedia/"
     },
     {
-      Name:"ButterFlix",
-      Desc:"Super 99.99% real epik movie",
+      Name:"butterFlix",
+      Desc:"super 99.99% real epik movie",
       Icon:"img/icons/ButterFlix.png",
       Link:"https://sites.google.com/view/butter-flix/"
     },
     {
-      Name:"Bulu",
-      Desc:"Hours of ads wiht 30 secund movie brake",
+      Name:"bulu",
+      Desc:"hours of ads wiht 30 secund movie brake",
       Icon:"img/icons/Bulu.png",
       Link:"https://sites.google.com/view/bulu/"
     },
     {
-      Name:"Butter+",
-      Desc:"Movie but wiht de plus",
+      Name:"butter+",
+      Desc:"movie but wiht de plus",
       Icon:"img/icons/ButterPlus.png",
       Link:"https://sites.google.com/view/butterplus"
     },
     {
-      Name:"Butterfiy",
-      Desc:"Want brake from ad? more ad",
+      Name:"butterfiy",
+      Desc:"want brake from ad? more ad",
       Icon:"img/icons/Butterfy.png",
       Link:"https://sites.google.com/bsd48.org/buterfiy/"
     },
     {
-      Name:"Butter Webstore",
-      Desc:"Get teh new buter for ur computehr",
+      Name:"LEADERBOARD",
+      Desc:"BSD CODE OF CONDUCT VIOLATION LEADERBOARD 🦅🇺🇲",
+      Icon:"img/catipillar.jpg",
+      Link:"https://butterdogceo.github.io/CODE-OF-CONDUCT-VIOLATION-LEADERBOARD/"
+    },
+    {
+      Name:"butter webstore",
+      Desc:"get teh new buter for ur computehr",
       Icon:"img/icons/Webstore.png",
       Link:"https://sites.google.com/view/butter-dog-search/apps/bdog-webstore/"
     },
     {
-      Name:"Bweeter",
-      Desc:"Most epic socal meda (beta)",
+      Name:"bweeter",
+      Desc:"most epic socal meda (beta)",
       Icon:"img/icons/Bweeter.png",
       Link:"https://bweeter.bettermode.io"
     },
     {
-      Name:"Bbay",
-      Desc:"Pay money for te thing you pay for",
+      Name:"bbay",
+      Desc:"pay money for te thing you pay for",
       Icon:"img/icons/Bbay.png",
       Link:"https://sites.google.com/bsd48.org/bbay"
     },
     {
-      Name:"ButterOS",
-      Desc:"The bestest and most epicest os for buter",
+      Name:"butteros",
+      Desc:"the bestest and most epicest os for buter",
       Icon:"img/icons/ButterOS.png",
       Link:"https://sites.google.com/bsd48.org/b-os/"
     },
     {
-      Name:"ButterDonalds",
-      Desc:"Cheapest food for healthiest option 👍",
+      Name:"butterdonalds",
+      Desc:"cheapest food for healthiest option 👍",
       Icon:"img/icons/ButterDonalds.png",
       Link:"https://sites.google.com/bsd48.org/bdonalds"
     },
     {
-      Name:"Calculater",
-      Desc:"Realy smort computor taht do math",
+      Name:"calculater",
+      Desc:"realy smart computor taht do math",
       Icon:"img/icons/Calculator.jpeg",
       Link:"tools/calculator"
     }
