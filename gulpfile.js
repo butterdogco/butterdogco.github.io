9910dcfe9b6d670a70891e80a536177367b81fbf
ButterDogCEO <100174775+butterdogceo@users.noreply.github.com>
2026-06-03 23:57:49 -0700

    Modify Gulp build process to actually work with non-html files

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,29 @@ const gulp = require('gulp');
 const htmlmin = require('gulp-htmlmin');
 const cleanCSS = require('gulp-clean-css');
 
-gulp.task('build', () => {
+function minifyHtml() {
   return gulp
-    .src('docs/**/*')
+    .src('docs/**/*.html', { base: 'docs' })
     .pipe(htmlmin({
       collapseWhitespace: true,
       removeComments: true,
       minifyJS: true,
       minifyCSS: true
     }))
+    .pipe(gulp.dest('dist'));
+}
+
+function minifyCss() {
+  return gulp
+    .src('docs/**/*.css', { base: 'docs' })
     .pipe(cleanCSS())
     .pipe(gulp.dest('dist'));
-});
+}
+
+function copyOtherFiles() {
+  return gulp
+    .src(['docs/**/*', '!docs/**/*.html', '!docs/**/*.css'], { base: 'docs' })
+    .pipe(gulp.dest('dist'));
+}
+
+gulp.task('build', gulp.parallel(minifyHtml, minifyCss, copyOtherFiles));
